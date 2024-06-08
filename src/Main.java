a126c349e89fe0a864716aac4c804d14f83a6950
rramola <ramolaryan@gmail.com>
2024-06-07 23:13:33 -0500

    Added feature creation option loop. Only works for bills and ending loop right now

--- src/Main.java
+++ src/Main.java
@@ -5,39 +5,53 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-        //Continue making bill loop boolean
-        Boolean anotherBill = true;
-
-        //List of bills
-        List<Bill> billList = new ArrayList<Bill>();
-
         //Scanners
-        Scanner makeNewBillInput = new Scanner((System.in));
+        //Intro scanners
+        Scanner plannerSelectionInput = new Scanner(System.in);
+
+
+        //bill scanners
+        Scanner makeNewBillInput = new Scanner(System.in);
         Scanner newBillNameInput = new Scanner(System.in);
         Scanner newBillAmountInput = new Scanner(System.in);
 
-        //Bill creation loop start
-        while(anotherBill) {
-            System.out.print("Would you like to make a new bill('Y' or 'N)? ");
-            String makeNewBill = makeNewBillInput.nextLine();
+        //Booleans
+        Boolean running = true;
 
-            //Check if a new bill will be added
-            if(makeNewBill.toLowerCase().equals("y")){
+        //Lists
+        List<Bill> billList = new ArrayList<Bill>();
 
-                //Bill creation input
-                System.out.print("Enter a name for the bill that is due > ");
-                String newBillName = newBillNameInput.nextLine();
-                System.out.print("Enter the amount due for this bill > ");
-                Double newBillAmount = newBillAmountInput.nextDouble();
 
-                //Bill object creation
-                Bill newBill = new Bill(newBillName, newBillAmount);
-
-                //Add bill to list
-                billList.add(newBill);
-                System.out.println(billList.size());
-            }else if(makeNewBill.toLowerCase().equals("n")){
-                anotherBill = false;
+        //Program start loop
+        while(running) {
+            //Feature Selection
+            System.out.print("What would you like to do. Make new [B]ill [T]ask [E]vent or [Q]uit > ");
+            String featureSelection = plannerSelectionInput.nextLine();
+            //Check which feature is selected
+            if (featureSelection.toLowerCase().equals("b")) {
+                Boolean anotherBill = true;
+                //Bill creation loop start
+                while (anotherBill) {
+                    System.out.print("Would you like to make a new bill('Y' or 'N)? ");
+                    String makeNewBill = makeNewBillInput.nextLine();
+                    //Check if a new bill will be added
+                    if (makeNewBill.toLowerCase().equals("y")) {
+                        //Bill creation input
+                        System.out.print("Enter a name for the bill that is due > ");
+                        String newBillName = newBillNameInput.nextLine();
+                        System.out.print("Enter the amount due for this bill > ");
+                        Double newBillAmount = newBillAmountInput.nextDouble();
+                        //Bill object creation
+                        Bill newBill = new Bill(newBillName, newBillAmount);
+                        //Add bill to list
+                        billList.add(newBill);
+                        System.out.println(billList.size());
+                    } else if (makeNewBill.toLowerCase().equals("n")) {
+                        anotherBill = false;
+                    }
+                }
+            } else if (featureSelection.toLowerCase().equals("q")){
+                running = false;
             }
         }
     }
