804d9744924359ecbcb980a992ec212b2585d458
rramola <ramolaryan@gmail.com>
2024-06-08 18:43:30 -0500

    Bill CRUD functionality completed, still need to implement error checking

--- src/Bill.java
+++ src/Bill.java
@@ -3,16 +3,27 @@ import java.util.Date;
 public class Bill {
     private String name;
     private Double amountDue;
-    public Bill(String name, Double amountDue)  {
+    private Boolean isPaid;
+    public Bill(String name, Double amountDue, boolean isPaid)  {
         this.name = name;
         this.amountDue = amountDue;
+        this.isPaid = isPaid;
     }
 
     public String getName(){
         return name;
     }
 
-    public Double getAmountDue(){
+    public Double getAmountDue() {
         return amountDue;
     }
+
+    public Boolean getIsPaid(){
+        return isPaid;
+    }
+
+    public void setIsPaid(Boolean isPaid){
+        this.isPaid = isPaid;
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -6,14 +6,17 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
         //Scanners
-        //Intro scanners
+        //intro scanners
         Scanner mainMenuSelectionInput = new Scanner(System.in);
         Scanner plannerSelectionInput = new Scanner(System.in);
-
         //bill scanners
+        Scanner billMenuSelectionInput = new Scanner(System.in);
         Scanner makeNewBillInput = new Scanner(System.in);
         Scanner newBillNameInput = new Scanner(System.in);
         Scanner newBillAmountInput = new Scanner(System.in);
+        Scanner payBillInput = new Scanner(System.in);
+        Scanner deleteBillInput = new Scanner(System.in);
+
         //task scanners
         Scanner makeNewTaskInput = new Scanner(System.in);
         Scanner newTaskNameInput = new Scanner(System.in);
@@ -22,43 +25,82 @@ public class Main {
         Scanner makeNewEventInput = new Scanner(System.in);
         Scanner newEventNameInput = new Scanner(System.in);
 
-        //Program loop boolean
-        Boolean running = true;
-
         //Lists
         List<Bill> billList = new ArrayList<Bill>();
         List<Task> taskList = new ArrayList<Task>();
         List<Event> eventList = new ArrayList<Event>();
 
+        //Program loop boolean
+        Boolean running = true;
+
+
         //Program start loop
         while(running) {
             //Feature Selection
-            System.out.print("Please select a menu option. [1]Bills, [2]Tasks, [3]Events, [4]Quit");
+            System.out.print("Please select a menu option. [1]Bills, [2]Tasks, [3]Events, [4]Quit > ");
             String mainMenuSelection = mainMenuSelectionInput.nextLine();
-
             //Bills menu
             if (mainMenuSelection.equals("1")){
-                Boolean anotherBill = true;
-                //Bill creation loop start
-                while (anotherBill) {
-                    System.out.print("Would you like to make a new bill('Y' or 'N)? ");
-                    String makeNewBill = makeNewBillInput.nextLine();
-                    //Check if a new bill will be added
-                    if (makeNewBill.toLowerCase().equals("y")) {
-                        //Bill creation input
-                        System.out.print("Enter a name for the bill that is due > ");
-                        String newBillName = newBillNameInput.nextLine();
-                        System.out.print("Enter the amount due for this bill > ");
-                        Double newBillAmount = newBillAmountInput.nextDouble();
-                        //Bill object creation
-                        Bill newBill = new Bill(newBillName, newBillAmount);
-                        //Add bill to list
-                        billList.add(newBill);
-                        System.out.println(billList.size());
-                    } else if (makeNewBill.toLowerCase().equals("n")) {
-                        anotherBill = false;
-                    } else {
-                        System.out.println("Invalid Selection");
+                Boolean billsMenuRunning = true;
+                while (billsMenuRunning) {
+                    System.out.print("Would you like to [1]Create, [2]View, [3]Pay, [4]Delete, or [5]Quit > ");
+                    String billMenuSelection = billMenuSelectionInput.nextLine();
+                    if (billMenuSelection.equals("1")) {
+                        Boolean anotherBill = true;
+                        //Bill creation loop start
+                        while (anotherBill) {
+                            System.out.print("Would you like to make a new bill('Y' or 'N)? ");
+                            String makeNewBill = makeNewBillInput.nextLine();
+                            //Check if a new bill will be added
+                            if (makeNewBill.toLowerCase().equals("y")) {
+                                //Bill creation input
+                                System.out.print("Enter a name for the bill that is due > ");
+                                String newBillName = newBillNameInput.nextLine();
+                                System.out.print("Enter the amount due for this bill > ");
+                                Double newBillAmount = newBillAmountInput.nextDouble();
+                                //Bill object creation
+                                Bill newBill = new Bill(newBillName, newBillAmount, false);
+                                //Add bill to list
+                                billList.add(newBill);
+                                System.out.println(billList.size());
+                            } else if (makeNewBill.toLowerCase().equals("n")) {
+                                anotherBill = false;
+                            } else {
+                                System.out.println("Invalid Selection");
+                            }
+                        }
+                    }else if(billMenuSelection.equals("2")){
+                        for (var i = 0; i < billList.size(); i++){
+                            var bill = billList.get(i);
+                            var paidStatus = "Paid";
+                            if (!bill.getIsPaid()) {
+                                paidStatus = "Unpaid";
+                            }
+                            System.out.println("Bill Name:" + " " + bill.getName() + "," + " " + "Amount Due:" + " " + bill.getAmountDue() + "," + " " + paidStatus);
+                        }
+                    }else if(billMenuSelection.equals("3")){
+                        System.out.print("Which bill would you like to pay? ");
+                        String payBillName = payBillInput.nextLine();
+                        for (var i = 0; i < billList.size(); i++){
+                            var bill = billList.get(i);
+                            if (bill.getName().toLowerCase().equals(payBillName)){
+                                System.out.println(bill.getName() + " " +  "Successfully Paid!");
+                                bill.setIsPaid(true);
+                            }
+
+                        }
+                    }else if(billMenuSelection.equals("4")){
+                        System.out.print("Which bill would you like to delete? ");
+                        String deleteBillName = deleteBillInput.nextLine();
+                        for (var i = 0; i < billList.size(); i++) {
+                            var bill = billList.get(i);
+                            if (bill.getName().toLowerCase().equals(deleteBillName)){
+                                System.out.println(bill.getName() + " " + "Successfully Deleted!");
+                                billList.remove(bill);
+                            }
+                        }
+                    } else if (billMenuSelection.equals("5")) {
+                        billsMenuRunning = false;
                     }
                 }
 
